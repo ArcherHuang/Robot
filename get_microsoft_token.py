51f5f3f2e7bfca835f976f68def40b5d8e0e6667
Archer <archerhuang@iii.org.tw>
2016-09-25 15:12:37 +0800

    add microsoft_text_to_speech code

--- get_microsoft_token.py
+++ get_microsoft_token.py
@@ -4,7 +4,7 @@ accessToken = ""
 wavBody = ""
 accessTokenHost = "api.cognitive.microsoft.com"
 tokenPath = "/sts/v1.0/issueToken"
-msBingSpeechAPIKey = ""
+msBingSpeechAPIKey = "9b4c82c9b8c342aa8beac43cf33a12bc"
 
 def getAccessToke():
 
--- microsoft_text_to_speech.py
+++ microsoft_text_to_speech.py
@@ -1,13 +1,15 @@
-# https://www.microsoft.com/cognitive-services/en-us/Speech-api/documentation/API-Reference-REST/BingVoiceOutput
-
+import get_microsoft_token
+import httplib, json, urllib
 import requests
+import uuid
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-token = "9b4c82c9b8c342aa8beac43cf33a12bc"
+getUUID = uuid.uuid4()
+accessToken = get_microsoft_token.getAccessToke()
 lang = "en-US"
 female = True
-text = "hello world"
+text = "I come from Taiwan"
 template = """
         <speak version='1.0' xml:lang='{0}'>
             <voice xml:lang='{0}' xml:gender='{1}' name='{2}'>
@@ -16,21 +18,25 @@ template = """
         </speak>
 """
 
-url = "https://speech.platform.bing.com/synthesize"
+speechHost = "speech.platform.bing.com"
 headers = {
     "Content-type": "application/ssml+xml",
     "X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
-    "Authorization": "Bearer " + token,
-    "X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
-    "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
-    "User-Agent": "OXFORD_TEST"
+    "Authorization": "Bearer " + accessToken,
+    "X-Search-AppId": str(getUUID).replace('-', ''),
+    "X-Search-ClientID": str(getUUID).replace('-', ''),
+    "User-Agent": "TTSForPython"
 }
 name = "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)"
 data = template.format(lang, "Female" if female else "Male", name, text)
 
-response = requests.post(url, data=data, headers=headers)
-
-if response.ok:
-    print response.content
-else:
-    raise response.raise_for_status()
+conn = httplib.HTTPSConnection(speechHost)
+conn.request("POST", "/synthesize", data, headers)
+response = conn.getresponse()
+ttsResponse = response.read()
+conn.close()
+print(response.status, response.reason)
+if response.status == 200:
+    f = open('test.wav','wb')
+    f.write(ttsResponse)
+    f.close()
