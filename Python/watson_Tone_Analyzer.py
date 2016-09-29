65f488c90024e54070fdbe812a2af12fc601d18a
Archer <archerhuang@iii.org.tw>
2016-09-30 07:53:41 +0800

    add Watson Tone Analyzer code

---
+++ Python/watson_Tone_Analyzer.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+import ConfigParser
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+userName = Config.get('ToneAnalyzer', 'userName')
+passWord = Config.get('ToneAnalyzer', 'passWord')
+
+tone_analyzer = ToneAnalyzerV3(
+    username = userName,
+    password = passWord,
+    version = '2016-02-11')
+
+print(json.dumps(tone_analyzer.tone(text='我好高興'), indent=2))
