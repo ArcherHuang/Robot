d093db22b1d2d448fd10507f5bc92e971fda160f
Archer <archerhuang@iii.org.tw>
2016-09-30 15:27:44 +0800

    add Watson Visual Recognition Code

---
+++ Python/watson_Visual_Recognition.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+
+# ******************************************************************************************
+# Import Package                                                                           #
+# ******************************************************************************************
+
+import ConfigParser
+import json
+from os.path import join, dirname
+from os import environ
+from watson_developer_cloud import VisualRecognitionV3
+
+# ******************************************************************************************
+# Get Service API Key and Check Local File or URL                                          #
+# ******************************************************************************************
+
+Config = ConfigParser.ConfigParser()
+Config.read("watson_Visual_Recognition_config.ini")
+apiKey = Config.get('VisualRecognition', 'apiKey')
+imageURL = "http://i.imgur.com/WyfYIwZ.jpg"
+
+# ******************************************************************************************
+# IBM Bluemix Watson Visual Recognition API - Classify an image                            #
+# ******************************************************************************************
+
+visual_recognition = VisualRecognitionV3('2016-05-20', api_key=apiKey)
+print(json.dumps(visual_recognition.classify(images_url=imageURL), indent=2))
+
+# ******************************************************************************************
+# IBM Bluemix Watson Visual Recognition API - Detect faces                                 #
+# ******************************************************************************************
+
+print(json.dumps(visual_recognition.detect_faces(images_url=imageURL), indent=2))
